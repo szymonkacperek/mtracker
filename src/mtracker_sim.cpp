f9c03f4b27a6b6f17a71ddcff2167d336d1b247a
szymonkacperek <szymon.kacperek96@gmail.com>
2023-11-25 17:29:22 +0100

    feat: add launchfile

--- src/mtracker_sim.cpp
+++ src/mtracker_sim.cpp
@@ -1,22 +1,27 @@
 ﻿#include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
+#include "geometry_msgs/Pose2D.h"
 
 void callback(const geometry_msgs::Twist& str) {
     ROS_INFO("received msg");
 }
 
-int main(int argc, char** argv) {
-    ros::init(argc, argv, "mtracker_sim");
-    ros::NodeHandle nh;
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "mtracker_sim");
+  ros::NodeHandle nh;
 
-    ros::Subscriber sub = nh.subscribe("/cmd_vel", 1, callback);
+  ros::Subscriber sub = nh.subscribe("/cmd_vel", 1, callback);
+  ros::Publisher pub = nh.advertise<geometry_msgs::Pose2D>("/pos", 5);
 
-    // while (ros::ok())
-    // {
-    //     ROS_INFO("i am alive");
-    // }
+  geometry_msgs::Pose2D msg;
 
-    ros::spin();
+  ros::Rate r(1);
 
-    return 0;
+  while (ros::ok()) {
+    ros::spinOnce();
+    pub.publish(msg);
+    r.sleep();
+  }
+
+  return 0;
 }
