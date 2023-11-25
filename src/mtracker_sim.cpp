476849b3aa04f04f3d8976ad243919b87917c311
szymonkacperek <szymon.kacperek96@gmail.com>
2023-11-25 11:50:18 +0100

    feat: add pure nodes

---
+++ src/mtracker_sim.cpp
@@ -0,0 +1,22 @@
+﻿#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+
+void callback(const geometry_msgs::Twist& str) {
+    ROS_INFO("received msg");
+}
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "mtracker_sim");
+    ros::NodeHandle nh;
+
+    ros::Subscriber sub = nh.subscribe("/cmd_vel", 1, callback);
+
+    // while (ros::ok())
+    // {
+    //     ROS_INFO("i am alive");
+    // }
+
+    ros::spin();
+
+    return 0;
+}
