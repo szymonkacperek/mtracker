efba32fbdf58255b9ea67d7b7b6d44d4bedf3af6
szymonkacperek <szymon.kacperek96@gmail.com>
2023-11-25 18:54:07 +0100

    feat: move to an object oriented version

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -1,23 +1,24 @@
 ﻿#include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
 #include "geometry_msgs/Pose2D.h"
+#include "mtracker.h"
 
-void callback(const geometry_msgs::Twist& str) {
-    ROS_INFO("received msg");
-}
-
-int main(int argc, char **argv) {
+int main(int argc, char** argv)
+{
   ros::init(argc, argv, "mtracker_sim");
   ros::NodeHandle nh;
 
-  ros::Subscriber sub = nh.subscribe("/cmd_vel", 1, callback);
+  MTracker mtracker;
+
+  ros::Subscriber sub = nh.subscribe("/cmd_vel", 1, &MTracker::SetOdometry, &mtracker);
   ros::Publisher pub = nh.advertise<geometry_msgs::Pose2D>("/pos", 5);
 
   geometry_msgs::Pose2D msg;
 
   ros::Rate r(1);
 
-  while (ros::ok()) {
+  while (ros::ok())
+  {
     ros::spinOnce();
     pub.publish(msg);
     r.sleep();
